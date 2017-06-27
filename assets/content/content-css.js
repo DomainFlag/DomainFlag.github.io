cf9f83420308992a4518e11013f4843831a4480d
DomainFlag <Cchivriga@hotmail.com>
2017-06-28 01:30:24 +0300

    changes to layout

---
+++ assets/content/content-css.js
@@ -0,0 +1,155 @@
+[
+  {
+    "content-header" : "COLORS",
+    "content-body" : [
+      {
+        "content-body-title" : "RGBa",
+        "content-body-info" : "",
+        "content-body-snippet" : "rgba(150, 150, 150, 0.5);"
+      }
+    ]
+  }, {
+    "content-header" : "FONTS",
+    "content-body" : [
+      {
+        "content-body-title" : "Fallback Fonts",
+        "content-body-info" : "",
+        "content-body-snippet" : "font-family: Garamond, Times, serif;"
+      }, {
+        "content-body-title" : "Line Height",
+        "content-body-info" : "",
+        "content-body-snippet" : "line-height: 1.5em;"
+      }, {
+        "content-body-title" : "Word Spacing",
+        "content-body-info" : "",
+        "content-body-snippet" : "word-spacing: 0.3em;"
+      }, {
+        "content-body-title" : "Letter Spacing",
+        "content-body-info" : "",
+        "content-body-snippet" : "letter-spacing: 0.3em;"
+      }
+    ]
+  }, {
+    "content-header" : "BOX MODEL",
+    "content-body" : [
+      {
+        "content-body-title" : "Min/Max",
+        "content-body-info" : "",
+        "content-body-snippet" : "min-width: 300px;"
+      }, {
+        "content-body-title" : "Overflow",
+        "content-body-info" : "",
+        "content-body-snippet" : "overflow: scroll/hidden;"
+      }
+    ]
+  }, {
+    "content-header" : "BORDERS",
+    "content-body" : [
+      {
+        "content-body-title" : "Border Style",
+        "content-body-info" : "",
+        "content-body-snippet" : "border-style: solid/dashed/dotted;"
+      }, {
+        "content-body-title" : "Shorthand",
+        "content-body-info" : "",
+        "content-body-snippet" : "border: 3px solid rgb(22, 77, 100);"
+      }, {
+        "content-body-title" : "Border Radius",
+        "content-body-info" : "",
+        "content-body-snippet" : "border-radius: 5px;"
+      }
+    ]
+  }, {
+    "content-header" : "CONTENT",
+    "content-body" : [
+      {
+        "content-body-title" : "Margin Auto",
+        "content-body-info" : "",
+        "content-body-snippet" : "margin: auto;(centering elements)"
+      }, {
+        "content-body-title" : "Resetting Default",
+        "content-body-info" : "",
+        "content-body-snippet" : "* { margin: 0; padding: 0; };"
+      }, {
+        "content-body-title" : "Display",
+        "content-body-info" : "",
+        "content-body-snippet" : "display: inline/inline-block/block;"
+      }, {
+        "content-body-title" : "Visibility",
+        "content-body-info" : "",
+        "content-body-snippet" : "visibility: hidden/none/visible;"
+      }
+    ]
+  }, {
+    "content-header" : "CHANGING THE BOX MODEL",
+    "content-body" : [
+      {
+        "content-body-title" : "Box Sizing",
+        "content-body-info" : "",
+        "content-body-snippet" : "* { box-sizing: border-box; }"
+      }
+    ]
+  }, {
+    "content-header" : "Layout",
+    "content-body" : [
+      {
+        "content-body-title" : "Position",
+        "content-body-info" : "",
+        "content-body-snippet" : "position: static/relative/absolute/fixed;"
+      }, {
+        "content-body-title" : "Offset Properties",
+        "content-body-info" : "",
+        "content-body-snippet" : "top/right/bottom/left: 20px;"
+      }, {
+        "content-body-title" : "Z-index",
+        "content-body-info" : "",
+        "content-body-snippet" : "z-index: 1/2/3/...;"
+      }, {
+        "content-body-title" : "Float",
+        "content-body-info" : "",
+        "content-body-snippet" : "float: left/right;"
+      }, {
+        "content-body-title" : "Clear",
+        "content-body-info" : "",
+        "content-body-snippet" : "clear: left/right/both/none;"
+      }
+    ]
+  }, {
+    "content-header" : "Adding Images",
+    "content-body" : [
+      {
+        "content-body-title" : "Box Sizing",
+        "content-body-info" : "",
+        "content-body-snippet" : "* { box-sizing: border-box; }"
+      }, {
+        "content-body-title" : "Centering Images",
+        "content-body-info" : "",
+        "content-body-snippet" : "display: block; margin: 0px auto;"
+      }, {
+        "content-body-title" : "Images",
+        "content-body-info" : "",
+        "content-body-snippet" : "background-image: url(\"your-link-here\");"
+      }, {
+        "content-body-title" : "Repeat",
+        "content-body-info" : "",
+        "content-body-snippet" : "background-repeat: repeat/repeat-x/repeat-y/no-repeat;"
+      }, {
+        "content-body-title" : "Position",
+        "content-body-info" : "",
+        "content-body-snippet" : "background-position: center;"
+      }, {
+        "content-body-title" : "Shorthand",
+        "content-body-info" : "",
+        "content-body-snippet" : "background: url(\"link\") no-repeat center;"
+      }, {
+        "content-body-title" : "Size",
+        "content-body-info" : "",
+        "content-body-snippet" : "background-size: cover/contain;"
+      }, {
+        "content-body-title" : "Linear gradient",
+        "content-body-info" : "",
+        "content-body-snippet" : "background-image: -webkit-linear-gradient(#666CCC, #BC1324);"
+      }
+    ]
+  }
+];
