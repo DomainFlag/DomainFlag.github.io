56cf19c3b90c9391049f0bf9856878a8b6308f2a
DomainFlag <Cchivriga@hotmail.com>
2017-06-30 01:22:27 +0300

    added new content

--- assets/scripts/index_script.js
+++ assets/scripts/index_script.js
@@ -168,7 +168,157 @@ var content_JSON = [
         ]
     }
 ],
-    [],
+    [
+        {
+            "content-header" : "GENERAL",
+            "content-body" : [
+                {
+                    "content-body-title" : "Number/String/Boolean Methods",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Number(); //->Converts a value to number"
+                }, {
+                    "content-body-title" : "isNaN Method",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "isNaN(\"hi\"); //->False"
+                }, {
+                    "content-body-title" : "typeof Operator",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "console.log(typeof \"hi\"); //->string"
+                }
+            ]
+        }, {
+            "content-header" : "ARRAYS",
+            "content-body" : [
+                {
+                    "content-body-title" : "Methods",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Push/Pop/Join - add/remove/stringfy"
+                }, {
+                    "content-body-title" : "indexOf & lastIndexOf",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1, 1].lastIndexOf/indexOf(1); //->1/0"
+                }, {
+                    "content-body-title" : "slice",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1, 2, 3].slice(1); //->[2, 3]"
+                }, {
+                    "content-body-title" : "concat",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1].concat([2, 3]); //->[1, 2, 3]"
+                }, {
+                    "content-body-title" : "forEach",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1, 2, 3].forEach(function(nb) { console.log(nb); } //-> 1 2 3"
+                }, {
+                    "content-body-title" : "Filter",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1, 2, 3].filter(function(nb) { return nb > 1; } //-> [2, 3]"
+                }, {
+                    "content-body-title" : "Map",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1, 2, 3].map(function(nb) { return nb+1; } //-> [2, 3, 4]"
+                }, {
+                    "content-body-title" : "Reduce",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "[1, 2, 3].reduce(function(sum, nb) { return sum+=nb; } //-> 6"
+                }
+            ]
+        }, {
+            "content-header" : "Objects",
+            "content-body" : [
+                {
+                    "content-body-title" : "Mutability",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Numbers, Strings, Booleans - Immutable & Arrays, Objects - Mutable"
+                }, {
+                    "content-body-title" : "Prototype",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Object.prototype.toDo = function() { ... };"
+                }, {
+                    "content-body-title" : "getPrototypeOf",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Object.getPrototypeOf(object).constructor.name;"
+                }, {
+                    "content-body-title" : "Keys",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "console.log(Object.keys({\"name\": \"A\", \"age\" : 1})); //-> [\"name\", \"age\"]"
+                }, {
+                    "content-body-title" : "Operator instanceof",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "console.log(dog instanceof Dog); //->true"
+                }
+            ]
+        }, {
+            "content-header" : "MATH",
+            "content-body" : [
+                {
+                    "content-body-title" : "Max & Min",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Math.max(1, 2, 3); //->3"
+                }, {
+                    "content-body-title" : "Sqrt & Pow",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Math.pow(4, 3); //->64"
+                }, {
+                    "content-body-title" : "Others",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "cos, sin, tan, PI, random, floor, ceil"
+                }
+            ]
+        }, {
+            "content-header" : "JSON",
+            "content-body" : [
+                {
+                    "content-body-title" : "Stringify & Parse",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "Converts data to this format and from it."
+                }
+            ]
+        }, {
+            "content-header" : "ERROR HANDLING",
+            "content-body" : [
+                {
+                    "content-body-title" : "Try, Catch, Throw",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "try { ... } catch(err) { ... throw(err) xxx };"
+                }
+            ]
+        }, {
+            "content-header" : "MODULES",
+            "content-body" : [
+                {
+                    "content-body-title" : "Export",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "if (typeof module != \"undefined\" && module.exports) module.exports = names;"
+                }, {
+                    "content-body-title" : "Import",
+                    "content-body-info" : "",
+                    "content-body-snippet" : "var names = require(\"./export.js\");"
+                }
+            ]
+        }, {
+            'content-header' : 'REGULAR EXPRESSIONS',
+            'content-body' : [
+                {
+                    'content-body-title' : 'Test Method',
+                    'content-body-info' : '',
+                    'content-body-snippet' : 'console.log(/.*[0-9]*a.*/.test("21412a")); //->true'
+                }, {
+                    'content-body-title' : 'Exec Method',
+                    'content-body-info' : '',
+                    'content-body-snippet' : 'console.log(/\d+/.exec("one two 100")); //-> ["100"]'
+                }, {
+                    'content-body-title' : 'Match Method',
+                    'content-body-info' : '',
+                    'content-body-snippet' : 'console.log("one two 100 200".match(/\d+/g)); //->["100", "200"]'
+                }, {
+                    'content-body-title' : 'Replace Method',
+                    'content-body-info' : '',
+                    'content-body-snippet' : 'console.log("Borobudur".replace(/[ou]/g, "a")); // → Barabadar'
+                }
+            ]
+        }
+    ],
     [
         {
             "content-header" : "Modules",
