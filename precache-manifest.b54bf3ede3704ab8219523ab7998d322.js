aaaebd1f98a9c9c0b33bf3645f1b47742b58b787
DomainFlag <Cchivriga@hotmail.com>
2019-02-07 22:05:38 +0100

    Updates

---
+++ precache-manifest.b54bf3ede3704ab8219523ab7998d322.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "c76817879a060588cc95a3b380911dd6",
+    "url": "/static/media/smoke.c7681787.png"
+  },
+  {
+    "revision": "e1f936593bc35dfc4886",
+    "url": "/static/css/main.498cc686.chunk.css"
+  },
+  {
+    "revision": "5505a09cfb5c17cdc467",
+    "url": "/static/js/1.5505a09c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76380d164c05788660ba682979002b3f",
+    "url": "/static/media/gradient-blue.76380d16.png"
+  },
+  {
+    "revision": "0815e194e2d8a36ca8d7ee69060795dd",
+    "url": "/static/media/gradient-black.0815e194.png"
+  },
+  {
+    "revision": "dd278ebcfd8fbf858bba93d466f2cf55",
+    "url": "/static/media/stardust.dd278ebc.jpg"
+  },
+  {
+    "revision": "48295c710f1631b0d325dca30b0371bf",
+    "url": "/static/media/resume.48295c71.pdf"
+  },
+  {
+    "revision": "0eafd8ee4ad8e21900c73f6bf967cbf7",
+    "url": "/static/media/logo-icon.0eafd8ee.svg"
+  },
+  {
+    "revision": "f9cb0a4bc823e9969afa458d7dff65e0",
+    "url": "/static/media/gradient.f9cb0a4b.png"
+  },
+  {
+    "revision": "e3b3bbec9a03f3964253de412e177430",
+    "url": "/static/media/front_1_2.e3b3bbec.png"
+  },
+  {
+    "revision": "7bdd15943a89966fe882ae9082bede8b",
+    "url": "/static/media/front_1.7bdd1594.png"
+  },
+  {
+    "revision": "0f79e3222d3360591916679fdbf5a1ad",
+    "url": "/static/media/design_3.0f79e322.png"
+  },
+  {
+    "revision": "a553f9fa2432f2f2f68a29a42489f507",
+    "url": "/static/media/front_2_1.a553f9fa.png"
+  },
+  {
+    "revision": "529667987a1638d632d825a31d2d4bcc",
+    "url": "/static/media/gradient-violet.52966798.png"
+  },
+  {
+    "revision": "4bcfc8efd6f8fc2ada32e337405582c7",
+    "url": "/static/media/logo.4bcfc8ef.png"
+  },
+  {
+    "revision": "4ef2c50adb785674f6dcc29716717912",
+    "url": "/static/media/screenshot.4ef2c50a.png"
+  },
+  {
+    "revision": "dc79b16c0a1b329c6c70cae91eb693fb",
+    "url": "/static/media/Video1.dc79b16c.mp4"
+  },
+  {
+    "revision": "6c68696d955ebe02ab5935de8be960a8",
+    "url": "/static/media/Video.6c68696d.mp4"
+  },
+  {
+    "revision": "e1f936593bc35dfc4886",
+    "url": "/static/js/main.e1f93659.chunk.js"
+  },
+  {
+    "revision": "9354af104e87e4041380686ff9dfa2a9",
+    "url": "/static/media/home.9354af10.svg"
+  },
+  {
+    "revision": "b5a29a8ec98a7bef5961dd63ab954614",
+    "url": "/static/media/album_1.b5a29a8e.jpg"
+  },
+  {
+    "revision": "52c1bc286ffd2273b13d417048e19767",
+    "url": "/static/media/album_10.52c1bc28.jpg"
+  },
+  {
+    "revision": "e626738302c950ac8c558b46be6ae526",
+    "url": "/static/media/album_11.e6267383.jpg"
+  },
+  {
+    "revision": "35d852c8b994abaf1cf565bdad946304",
+    "url": "/static/media/album_12.35d852c8.jpg"
+  },
+  {
+    "revision": "9d4a6987024fdd2db1e1150baf6207a8",
+    "url": "/static/media/album_13.9d4a6987.png"
+  },
+  {
+    "revision": "02cb44999b0fb142c9f37059e91a99d6",
+    "url": "/static/media/album_14.02cb4499.jpg"
+  },
+  {
+    "revision": "38f1cec5be188438262f47a7545abf75",
+    "url": "/static/media/album_15.38f1cec5.jpg"
+  },
+  {
+    "revision": "0dc0b034c9a2413e4f6e129466b2de2c",
+    "url": "/static/media/album_16.0dc0b034.png"
+  },
+  {
+    "revision": "9d4ef80b3e124b213c2b9c0f22f42310",
+    "url": "/static/media/album_17.9d4ef80b.jpg"
+  },
+  {
+    "revision": "92952d7fd67a52d6c980d8f0ba4f3586",
+    "url": "/static/media/album_2.92952d7f.jpg"
+  },
+  {
+    "revision": "7b5ad52cfc5899ad6c6dce5be0b33f35",
+    "url": "/static/media/album_3.7b5ad52c.png"
+  },
+  {
+    "revision": "202db674901508540b1d24fccb2d02f2",
+    "url": "/static/media/album_4.202db674.png"
+  },
+  {
+    "revision": "23a82c6bc28b3955959730476e234122",
+    "url": "/static/media/album_6.23a82c6b.png"
+  },
+  {
+    "revision": "095fd399191d8b51ddf6ffafbeea7c05",
+    "url": "/static/media/album_7.095fd399.jpg"
+  },
+  {
+    "revision": "d2ff0cd1a09adf67fdc4bdf385140c0e",
+    "url": "/static/media/album_8.d2ff0cd1.png"
+  },
+  {
+    "revision": "d46517c673eac643df453ecc169f1f26",
+    "url": "/static/media/album_9.d46517c6.jpg"
+  },
+  {
+    "revision": "3a50612e7d20af50a2ef5e59a1794ec6",
+    "url": "/index.html"
+  }
+];
